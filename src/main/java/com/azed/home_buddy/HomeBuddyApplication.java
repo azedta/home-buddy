36503e7e99df98a976467fb10ee9cc00822f16de
Aziz Tazrout <aztazrout@gmail.com>
2025-03-22 16:09:12 -0400

    Initialize the Home Buddy Project

---
+++ src/main/java/com/azed/home_buddy/HomeBuddyApplication.java
@@ -0,0 +1,13 @@
+package com.azed.home_buddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeBuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeBuddyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/azed/home_buddy/HomeBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azed.home_buddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeBuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
